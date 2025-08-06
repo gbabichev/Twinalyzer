26ca7f67a30962ea66ce6af8d0d877dfaf870715
George Babichev <gbabichev1@gmail.com>
2025-08-06 14:53:30 -0400

    Initial Commit

---
+++ ImageGuru/ImageGuruApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImageGuruApp.swift
+//  ImageGuru
+//
+//  Created by George Babichev on 8/6/25.
+//
+
+import SwiftUI
+
+@main
+struct ImageGuruApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
