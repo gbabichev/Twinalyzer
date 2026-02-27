c3743e45a9738d65f3de3b365dc8181600c7438c
George Babichev <gbabichev1@gmail.com>
2026-02-27 15:58:57 -0500

    Code cleanup

--- Twinalyzer/Models/ImageComparisonModels.swift
+++ Twinalyzer/Models/ImageComparisonModels.swift
@@ -12,16 +12,16 @@ enum AnalysisMode: String, CaseIterable, Identifiable, RawRepresentable {
     case deepFeature = "Enhanced Scan"
     case perceptualHash = "Basic Scan"
     
-    public var id: String { rawValue }
+    var id: String { rawValue }
     
-    public var rawValue: String {
+    var rawValue: String {
         switch self {
         case .deepFeature: return "Enhanced Scan"
         case .perceptualHash: return "Basic Scan"
         }
     }
     
-    public init?(rawValue: String) {
+    init?(rawValue: String) {
         switch rawValue {
         case "Enhanced Scan": self = .deepFeature
         case "Basic Scan": self = .perceptualHash
@@ -32,7 +32,7 @@ enum AnalysisMode: String, CaseIterable, Identifiable, RawRepresentable {
 
 // MARK: - Hierarchical Result (unchanged)
 struct ImageComparisonResult: Identifiable, Sendable, Equatable, Hashable {
-    public let id: UUID
+    let id: UUID
     let reference: String
     let similars: [(path: String, percent: Double)]
     
@@ -42,11 +42,11 @@ struct ImageComparisonResult: Identifiable, Sendable, Equatable, Hashable {
         self.similars = similars
     }
     
-    public static func == (lhs: ImageComparisonResult, rhs: ImageComparisonResult) -> Bool {
+    static func == (lhs: ImageComparisonResult, rhs: ImageComparisonResult) -> Bool {
         lhs.id == rhs.id
     }
     
-    public func hash(into h: inout Hasher) {
+    func hash(into h: inout Hasher) {
         h.combine(id)
     }
 }
